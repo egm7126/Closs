1f3da737ebfe5485854ecce297251286897a9c7d
buseop.jo <egm7126@naver.com>
2024-03-22 14:14:41 +0900

    trying to make sense t4 selectBonadeDevice file

---
+++ lib/tests/for bluetooth/t4/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'MainPage.dart';
+
+void main() => runApp(ExampleApplication());
+
+class ExampleApplication extends StatelessWidget {
+  const ExampleApplication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainPage());
+  }
+}
